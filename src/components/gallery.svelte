c37a28c218c1f5687212f786769d8ca9d950f299
William Owen <william.owen.bsc@gmail.com>
2023-12-09 21:25:07 -0800

    save

---
+++ src/components/gallery.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<div class="d-grid galleryGrid rounded">
+  <slot />
+</div>
+
+<style>
+  :global(.d-grid) {
+    display: grid;
+  }
+  .galleryGrid {
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 10px;
+    overflow: hidden;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,9 @@
 <script>
+  import Gallery from "../components/gallery.svelte";
   import GalleryImage from "../components/galleryImage.svelte";
 </script>
 
-<!-- <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap" />
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div> -->
-
-<div class="d-grid galleryGrid rounded">
+<Gallery>
   <GalleryImage
     src="https://media.istockphoto.com/id/1150481340/vector/realistic-mountains-landscape-morning-wood-panorama-pine-trees-and-mountains-silhouettes.jpg?s=1024x1024&w=is&k=20&c=EAoY9ekkKfhIgNmAuCDhQuk-F7hDFhyhk0cixjF53ts="
   />
@@ -35,15 +16,4 @@
   <GalleryImage
     src="https://media.istockphoto.com/id/1153318151/photo/colorful-wedding-flower-arrangement.jpg?s=1024x1024&w=is&k=20&c=EdIHD3N8OozlOAnhE-jljzL0j-kTc2ikiEVfVmoyRww="
   />
-</div>
-
-<style>
-  :global(.d-grid) {
-    display: grid;
-  }
-  .galleryGrid {
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 10px;
-    overflow: hidden;
-  }
-</style>
+</Gallery>
