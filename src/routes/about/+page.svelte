ecfc82b674f8b0cf17af4321a8cfd0a9195a4f80
William Owen <william.owen.bsc@gmail.com>
2023-12-09 21:37:02 -0800

    save

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,32 +1,39 @@
-<section class="about-section">
+<section class="about-section px-1">
   <div class="row">
-    <div class="col-6">
+    <div class="">
       <img
         src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
       />
-    </div>
-    <div class="about-description col-6">
-      <div>
+      <div class="text-container">
+        <h3>About Me</h3>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur ut
-        laudantium fuga reprehenderit, alias mollitia omnis, aliquid et sit a
-        eaque quidem totam atque sint temporibus debitis officia in modi,
-        ducimus fugiat animi quos nostrum. Quidem nulla cupiditate blanditiis
-        dignissimos natus facilis omnis, ullam magni minima id sit odit
-        doloribus numquam recusandae incidunt eum? Vero quisquam maxime
-        explicabo quas consequatur ab? Quibusdam pariatur, nobis quod labore
-        cupiditate dolores beatae debitis accusamus animi non obcaecati, natus
+        laudantium fuga reprehenderit, alias mollitia omnis, aliquid et sit a eaque
+        quidem totam atque sint temporibus debitis officia in modi, ducimus fugiat
+        animi quos nostrum. Quidem nulla cupiditate blanditiis dignissimos natus
+        facilis omnis, ullam magni minima id sit odit doloribus numquam recusandae
+        incidunt eum? Vero quisquam maxime explicabo quas consequatur ab? Quibusdam
+        pariatur, nobis quod labore cupiditate dolores beatae debitis accusamus animi
+        non obcaecati, natus
+        <div class="about-description d-flex justify-content-center mt-5">
+          <a href="#goToSite"><i class="fa fa-instagram"></i></a>
+          <a href="#goToSite"><i class="fa fa-facebook"></i></a>
+          <a href="#goToSite"><i class="fa fa-twitter"></i></a>
+        </div>
       </div>
-      <a href="#goToSite"><i class="fa fa-instagram"></i></a>
-      <a href="#goToSite"><i class="fa fa-facebook"></i></a>
-      <a href="#goToSite"><i class="fa fa-twitter"></i></a>
     </div>
   </div>
 </section>
 
 <style lang="scss">
+  .text-container {
+    height: 100%;
+  }
   img {
-    width: 100%;
+    width: 40%;
+    min-width: 200px;
+    float: left;
+    padding-right: 20px;
   }
   .fa {
     padding: 13px;
