440bad4ac3dbdea0c7f2a0943ed5dbdaf7c96a1d
William Owen <william.owen.bsc@gmail.com>
2023-12-17 09:41:35 -0800

    save

--- src/components/Gallery/gallery.svelte
+++ src/components/Gallery/gallery.svelte
@@ -11,6 +11,7 @@
   }
   .galleryGrid {
     grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 40px;
     /* And other times this */
     @media (max-width: 750px) {
       grid-template-columns: 1fr 1fr;
@@ -18,7 +19,6 @@
     /* @media (max-width: 650px) {
       grid-template-columns: 1fr;
     } */
-    grid-gap: 10px;
     overflow: hidden;
   }
 </style>
