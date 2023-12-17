400f535f896f2712947950e49d936d6a80b2b7b6
William Owen <william.owen.bsc@gmail.com>
2023-12-17 13:44:40 -0800

    save

--- src/components/Modals/commonPaintingModalBody.svelte
+++ src/components/Modals/commonPaintingModalBody.svelte
@@ -19,9 +19,16 @@
       <label for="" class="mt-2">Description</label>
       <textarea name="" id="" class="w-100 form-control description"></textarea>
     </div>
-    <div class="field d-flex flex-column">
-      <label for="" class="mt-2">Cost</label>
-      <input name="" id="" class="w-100 form-control cost" />
+    <div class="input-group mt-4">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id=""><i class="fa fa-dollar"></i></span>
+      </div>
+      <input
+        type="text"
+        class="form-control cost"
+        placeholder="Cost"
+        aria-label="Cost"
+      />
     </div>
   </div>
 </div>
